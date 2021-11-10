00fdf112397d564ab2ee84e375b40ea36d3ac611
Florian Salaun <florian.salaun@nokia.com>
2021-11-10 14:14:08 +0100

    First commit
    Commit first files to have release 0.1

---
+++ Logger.py
@@ -0,0 +1,49 @@
+import logging
+import logging.handlers
+
+
+class Logger:
+    def __init__(self, logger_name,
+                 logfile=None,
+                 verbose=False):
+        self.logger = logging.getLogger(logger_name)
+        if len(self.logger.handlers) != 0:
+            return
+        self.logger.setLevel(logging.DEBUG)
+        if verbose == True:
+            self.enableVerbose()
+        self.addFileHandler(logfile)
+
+    def enableVerbose(self):
+        for hdlr in self.logger.handlers:
+            if type(hdlr) == logging.StreamHandler:
+                hdlr.setLevel(logging.INFO)
+                return
+        stdout_handler = logging.StreamHandler()
+        stdout_formatter = logging.Formatter(
+            '%(levelname)s %(name)s: %(message)s')
+        stdout_handler.setFormatter(stdout_formatter)
+        stdout_handler.setLevel(logging.INFO)
+        self.logger.addHandler(stdout_handler)
+
+    def disableVerbose(self):
+        for hdlr in self.logger.handlers:
+            if type(hdlr) == logging.StreamHandler:
+                hdlr.setLevel(logging.CRITICAL+1)
+
+    def addFileHandler(self, logfile, level=logging.DEBUG):
+        if logfile != None:
+            handler = logging.handlers.RotatingFileHandler(logfile,
+                                                           maxBytes=52444160,
+                                                           backupCount=3)
+            handler.setLevel(level)
+            formatstr = ('%(asctime)s %(levelname)s %(filename)s: %(message)s')
+            formatter = logging.Formatter(formatstr)
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+
+    def getLogger(self):
+        return self.logger
+
+
+logger = Logger('Logger', 'NetConfParser.log', True).getLogger()
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# netconf-parser
+# Building NetConfParser
+NetConfParser can be built for Windows with pyinstaller
+        pyinstaller  --windowed --icon=fs.ico netconfparser.py --add-data "fs_ico_encoded;."
+It will give an exe as output in dist folder
+Please zip it if you want to distribute it
+For Linux, you can use python netconfparser.py directly
+
+# Using NetConfParser
+Usage of NetConfParser is pretty straightforward
+
+# Loading log file
+You can either paste the entire content of the log file in the upper right text box
+Or use the load button which will open a dialog box
+
+# Parsing log file
+After loading the log file, you will have it pasted in the upper right text box
+Just press the Paste button just under the text box
+A tree will appear on the left frame
+
+# Reading the output tree
+Output tree is on the left frame
+In yellow you have the hello and the notifications
+In blue the rpcs coming from ODU
+In green the rpc responses coming from ORU
+You can expand the content of all messages by clicking the + on the left
+
+# Generating SCT file to reproduce PRs -- Not usable
+Under the lower right text box press the button Generate SCT
+Text will appear in the lower right text box
+Use then Copy To Clipboard button to copy
+
